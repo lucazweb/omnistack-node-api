ee217d0b2e3099c17a11beb6736446d3da77c729
lucazweb <lucazwebmail@gmail.com>
2020-03-25 18:42:09 -0300

    Adds cors module to application

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 app.listen(3333, () => console.log('server is running'));
